8fe7525dcf3d1eb6c96a9e5b411ea8e5ff323fd1
Miles Wells <mileswells93@gmail.com>
2024-02-19 00:21:14 -0500

    working remaining

---
+++ src/components/line-parse-textrea.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { HTMLTextareaAttributes } from 'svelte/elements';
+
+	type LineParseTextareaProps = Omit<HTMLTextareaAttributes, 'onchange' | 'value'> & {
+		onLinesParsed: (lines: string[]) => void;
+	};
+
+	let { onLinesParsed, ...textareaProps } = $props<LineParseTextareaProps>();
+
+	let value: string;
+
+	function parseLinesFromValue(value: string) {
+		return value
+			.trim()
+			.split(/\r\n|\r|\n/)
+			.map(parseLine)
+			.filter((line) => line.length > 0);
+	}
+
+	const numberedRegex = /^\d /;
+
+	function parseLine(line: string) {
+		if (!numberedRegex.test(line)) return line.trim();
+
+		return line.replace(numberedRegex, '').trim();
+	}
+</script>
+
+<textarea
+	bind:value
+	onchange={() => onLinesParsed(parseLinesFromValue(value))}
+	{...textareaProps}
+/>
+
+<style>
+	textarea {
+		height: 45rem;
+		resize: vertical;
+		width: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import LineParseTextrea from '../components/line-parse-textrea.svelte';
+
+	let cubeList = $state<string[]>([]);
+	let removedCards = $state<string[]>([]);
+
+	let remainingCards = $derived.by(() => {
+		const cubeSet = new Set(cubeList);
+		removedCards.forEach((card) => cubeSet.delete(card));
+		return Array.from(cubeSet);
+	});
+</script>
+
+<main>
+	<section>
+		<section class="header">
+			<h2>Cube List</h2>
+
+			<p class="count">{cubeList.length} cards</p>
+		</section>
+
+		<LineParseTextrea onLinesParsed={(lines) => (cubeList = lines)} />
+	</section>
+
+	<section>
+		<section class="header">
+			<h2>Removed Cards</h2>
+
+			<p class="count">{removedCards.length} cards</p>
+		</section>
+
+		<LineParseTextrea onLinesParsed={(lines) => (removedCards = lines)} />
+	</section>
+
+	<section>
+		<section class="header">
+			<h2>Remaining Cards</h2>
+
+			<Icon icon="material-symbols:content-copy-outline" width={30} />
+
+			<p class="count">{remainingCards.length} cards</p>
+		</section>
+
+		<section class="remaining">
+			{#each remainingCards as card}
+				<div>{card}</div>
+			{/each}
+		</section>
+	</section>
+</main>
+
+<style lang="scss">
+	main {
+		display: grid;
+		gap: 2rem;
+		grid-template-columns: repeat(3, 1fr);
+		padding: 2rem;
+
+		.header {
+			align-items: center;
+			display: flex;
+			gap: 0.5rem;
+
+			.count {
+				margin-left: auto;
+			}
+
+			:global(svg) {
+				cursor: pointer;
+			}
+		}
+
+		.remaining {
+			height: 45rem;
+			overflow: auto;
+		}
+	}
+</style>
